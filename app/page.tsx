2f0429aea1b2e81bbfabec8ce844346ef436ef42
Romain <romain.sanieres@gmail.com>
2024-08-08 19:42:54 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,47 @@
+import { auth, signIn, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
-  const createAdmin = async () => {
-    "use server"
-    console.log("test")
-  };
+  const session = await auth();
+
+  if (!session)
+    return (
+      <main className="flex justify-center items-center w-full min-h-dvh">
+        <div className="flex flex-col gap-y-2 w-96">
+          <p className="text-4xl underline">Boilerpla.te</p>
+          <Link href={"/dashboard"}>Dash</Link>
+          <div className="flex gap-x-2">
+            <form className="w-full space-y-2">
+              <Button
+                formAction={async () => {
+                  "use server";
+                  await signIn("google");
+                }}
+              >
+                Login
+              </Button>
+            </form>
+          </div>
+        </div>
+      </main>
+    );
+
   return (
     <main className="flex justify-center items-center w-full min-h-dvh">
       <div className="flex flex-col gap-y-2 w-96">
-        <p className="text-4xl underline">Boilerpla.te</p>
+        <p className="text-4xl underline">{session.user?.email}</p>
+        <Link href={"/dashboard"}>Dash</Link>
+
         <div className="flex gap-x-2">
           <form className="w-full space-y-2">
             <Button
-              
               formAction={async () => {
                 "use server";
-                await createAdmin();
+                await signOut();
               }}
             >
-              Add Admin
+              Log out
             </Button>
           </form>
         </div>
--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+// Avoid multiple requests
 import { db } from "./db";
 
 export const {
@@ -11,5 +12,16 @@ export const {
 } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      // Force select account
+      authorization: {
+        params: {
+          prompt: "select_account",
+        },
+      },
+    }),
+  ],
 });
